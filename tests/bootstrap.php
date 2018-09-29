501d2ff773cf8ed303ead1f35c20678a80d1c3e9
Martijn van der Lee <martijn@vanderlee.com>
2018-09-29 13:06:43 +0200

    Test travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,7 @@ spl_autoload_register(function ($class) {
     static $prefix = 'Vanderlee\\Comprehend\\';
     if (stripos($class, $prefix) === 0) {
         $file = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . str_ireplace($prefix, '', $class) . '.php';
+var_dump($file);
         if (is_file($file)) {
             /** @noinspection PhpIncludeInspection */
             require_once $file;
