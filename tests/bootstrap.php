7158583843e078d35829dc9cf2a7b13593fb96ef
Martijn van der Lee <martijn@vanderlee.com>
2018-09-29 13:17:40 +0200

    Fix bootstrap and namespaces

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,6 @@ spl_autoload_register(function ($class) {
     static $prefix = 'Vanderlee\\Comprehend\\';
     if (stripos($class, $prefix) === 0) {
         $file = str_replace('\\', DIRECTORY_SEPARATOR, dirname(__DIR__) . '\\src\\' . str_ireplace($prefix, '', $class) . '.php');
-var_dump($file);
         if (is_file($file)) {
             /** @noinspection PhpIncludeInspection */
             require_once $file;
